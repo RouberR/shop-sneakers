b873a692479d716188000f9f88b60a6ca4005c55
RouberR <Rouber1998@gmail.com>
2021-10-22 20:22:18 +0300

    deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <header className="d-flex justify-between align-center p-40">
 
-      <Link to="/">
+      <Link to="">
         <div className="d-flex align-center ">
           <img width={70} height={60} src="img/logo.png" alt="Logo" />
           <div className="headerInfo">
@@ -24,7 +24,7 @@ const Header = (props) => {
           <span>{totaPrice}$</span>
         </li>
         <li className="mr-30 cu-p">
-          <Link to="/favorites">
+          <Link to="favorites">
             <img
               width={20}
               height={20}
@@ -34,7 +34,7 @@ const Header = (props) => {
           </Link>
         </li>
         <li>
-          <Link to="/orders">
+          <Link to="orders">
             <img width={20} height={20} src="img/user.svg" alt="user" />
           </Link>
         </li>
